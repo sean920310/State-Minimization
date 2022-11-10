aee50a94a755f07f4636f6d14b0b6f0fa9e627f0
稚翔 曾 <sean920310@gmail.com>
2022-11-10 20:40:31 +0800

    one input reduce done

--- State Minimization/State Minimization.cpp
+++ State Minimization/State Minimization.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <unordered_set>
 
 using namespace std;
 
@@ -14,6 +15,26 @@ typedef struct State
 	string lowState, highState;
 	bool lowOutput, highOutput;
 }State;
+
+namespace std
+{
+	template<>
+	struct hash<State>
+	{
+		size_t operator()(const State& key) const
+		{
+			return hash<std::string>()(key.name);
+		}
+	};
+	template<>
+	struct equal_to<State>
+	{
+		bool operator()(const State& lhs,const State& rhs) const
+		{
+			return lhs.name == rhs.name;
+		}
+	};
+}
 
 typedef struct StateOutput
 {
@@ -27,6 +48,8 @@ typedef struct StateOutput
 		return (lowState == rhs.lowState) && (highState == rhs.highState) && (lowOutput == rhs.lowOutput) && (highOutput == rhs.highOutput);
 	}
 }StateOutput;
+
+
 
 typedef struct pairState
 {
@@ -143,20 +166,14 @@ int main(int argc, char* argv[])
 //=========================================================================
 void reduceState(map<string, State>& states)
 {
-	vector<pairState> implicantTable;
-	set<string> incompatibleState;
-	map<pair<bool, bool>,int> outputState;
+	set<pair<bool, bool>> outputType;
 	vector<pair<string, State>> tempState;
 	for (const auto& v : states)
 	{
 		//copy to temp
 		tempState.push_back(v);
 
-		//find incompatible pair
-		if (outputState.count(pair<bool, bool>(v.second.lowOutput, v.second.highOutput)))
-			outputState[pair<bool, bool>(v.second.lowOutput, v.second.highOutput)]++;
-		else
-			outputState[pair<bool, bool>(v.second.lowOutput, v.second.highOutput)] = 1;
+		outputType.insert(pair<bool, bool>(v.second.lowOutput, v.second.highOutput));
 	}
 
 	while (true)
@@ -195,44 +212,122 @@ void reduceState(map<string, State>& states)
 		}
 	}
 
-
-	//setup implication table
-	for (int i = 0; i < tempState.size()-1; i++)
+	//reduse term by classify output
+	vector<vector<State>> classifyOutput(outputType.size(),vector<State>());
+	vector<State> temp;
+	for (const auto& state : tempState)
 	{
-		for (int j = i + 1; j < tempState.size(); j++)
+		int i = 0;
+		for (const auto& type: outputType)
 		{
-			implicantTable.push_back(pairState(tempState[i].second, tempState[j].second,false));
+			if (pair<bool, bool>(state.second.lowOutput, state.second.highOutput) == type)
+			{
+				classifyOutput[i].push_back(state.second);
+				break;
+			}
+			i++;
+		}
+	}
+	//將不同輸出的set分成不同的set
+	while (true)
+	{
+		map<string, pair<int,int>> outputIndex;
+		for (int i = 0; i < classifyOutput.size(); i++)
+		{
+			for (int j = 0; j < classifyOutput[i].size(); j++)
+			{
+				pair<int, int> classify;
+				int lowIndex, highIndex;
+				for (int m = 0; m < classifyOutput.size(); m++)
+				{
+					for (int n = 0; n < classifyOutput[m].size(); n++)
+					{
+						if (classifyOutput[i][j].lowState == classifyOutput[m][n].name)
+							lowIndex = m;
+						if (classifyOutput[i][j].highState == classifyOutput[m][n].name)
+							highIndex = m;
+					}
+				}
+				classify.first = lowIndex;
+				classify.second = highIndex;
+
+				outputIndex[classifyOutput[i][j].name] = classify;
+			}
+		}
+
+		vector<vector<State>> newClassifyOutput;
+		for (int i = 0; i < classifyOutput.size(); i++)
+		{
+			if (classifyOutput[i].size() > 1)	//重新分類之後放入新的
+			{
+				vector<pair<pair<int, int>,unordered_set<State>>> outputSetCount;
+				//不同的輸出結果分類
+				for (int j = 0; j < classifyOutput[i].size(); j++)
+				{
+					bool find = false;
+					for (auto& set : outputSetCount)
+					{
+						if(set.first == outputIndex[classifyOutput[i][j].name])
+						{
+							set.second.insert(classifyOutput[i][j]);
+							find = true;
+						}
+					}
+					if (!find)
+					{
+						outputSetCount.push_back(pair<pair<int, int>, unordered_set<State>>(outputIndex[classifyOutput[i][j].name], unordered_set<State>()));
+						outputSetCount[outputSetCount.size()-1].second.insert(classifyOutput[i][j]);
+					}
+				}
+				//把不同的結果放到新的set
+				for (const auto& outputSet : outputSetCount)
+				{
+					vector<State> newSet;
+					for (const auto& state : outputSet.second)
+					{
+						newSet.push_back(state);
+					}
+					newClassifyOutput.push_back(newSet);
+				}
+			}
+			else	//如果只有一個就直接放入新的
+				newClassifyOutput.push_back(classifyOutput[i]);
+		}
+		//如果沒有再分出新的set就跳出迴圈
+		if (classifyOutput.size() != newClassifyOutput.size())
+		{
+			classifyOutput = newClassifyOutput;
+		}
+		else
+		{
+			classifyOutput = newClassifyOutput;
+			break;
 		}
 	}
 
-	//find incompatible pair
-	for (const auto& outputVal : outputState)
+	//取代所有重複的term
+	for (int i = 0; i < classifyOutput.size(); i++)
 	{
-		if (outputVal.second == 1)
+		if (classifyOutput.size() != 1)
 		{
-			for (const auto& stateVal : tempState)
+			for (int j = 1; j < classifyOutput[i].size(); j++)
 			{
-				if (pair<bool, bool>(stateVal.second.lowOutput, stateVal.second.highOutput) == outputVal.first)
+				for (int m = 0; m < tempState.size(); m++)
 				{
-					incompatibleState.insert(stateVal.first);
-					break;
+					if (tempState[m].second.highState == classifyOutput[i][j].name)
+						tempState[m].second.highState = classifyOutput[i][0].name;
+					if (tempState[m].second.lowState == classifyOutput[i][j].name)
+						tempState[m].second.lowState = classifyOutput[i][0].name;
+					if (tempState[m].first == classifyOutput[i][j].name)
+					{
+						tempState.erase(tempState.begin() + m);
+						m--;
+					}
 				}
 			}
 		}
 	}
 
-	//set incompatible pair to true
-	for (auto& val : implicantTable)
-	{
-		for (const auto& incompState : incompatibleState)
-		{
-			if (val.stateA.name == incompState || val.stateB.name == incompState)
-			{
-				val.incompatible = true;
-			}
-		}
-	}
+	//TODO: output;
 
-	//TODO:
-	//find all incompatible
 }
